5a9575e7efd1365f54c72f4f5dfa6931873dc6e6
wjm <wondaero@fuz.co.kr>
2026-09-18 16:29:00 +0900

    팝업 닫기버튼 위치 수정

--- src/components/custom/dialog/GenericDialog2.vue
+++ src/components/custom/dialog/GenericDialog2.vue
@@ -95,7 +95,7 @@ const closeClass = computed(() =>
   deviceStyle(
     {
       pc: `absolute right-[40px] top-[30px] focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4`,
-      mobile: 'size-5',
+      mobile: `size-5 top-[${props.type === 'full' ? 18 : 32}px]}`,
     },
     props.device,
   ),
